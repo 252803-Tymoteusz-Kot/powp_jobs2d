54af7424cba3681f700d7cde957510947d1b58be
Tymoteusz Kot <252803@edu.p.lodz.pl>
2026-03-11 17:57:57 +0100

    3.2 - Zad 2

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -40,7 +40,7 @@ public class TestJobs2dPatterns {
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
 		Job2dDriver testDriver = new DrawPanelAdapter();
-		DriverFeature.addDriver("Buggy Simulator", testDriver);
+		DriverFeature.addDriver("DrawPanelAdapter", testDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
